a1d0aa21a1f6162f7519316e20d73ade8968e08c
Maros Dániel <marosdaniel@gmail.com>
2024-03-31 22:14:29 +0200

    footer fixes

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,3 +1,26 @@
+// export const useFooterStyles = () => {
+//   const isDrawerOpen = useGlobalState().isDrawerOpen;
+//   const footerStyles = {
+//     display: 'flex',
+//     justifyContent: 'space-between',
+//     alignItems: 'center',
+//     height: '60px',
+//     // width: `${isDrawerOpen ? 'calc(100% - 240px)' : 'calc(100% - 64px)'}`,
+//     width: {
+//       xs: `${isDrawerOpen ? 'calc(100% - 240px)' : 'calc(100% - 64px)'}`,
+//       sm: `${isDrawerOpen ? 'calc(100% - 240px)' : 'calc(100% - 64px)'}`,
+//       md: `${isDrawerOpen ? 'calc(100% - 240px)' : 'calc(100% - 64px)'}`,
+//     },
+//     marginLeft: `${isDrawerOpen ? '240px' : '64px'}`,
+//     backgroundColor: 'transparent',
+//     boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.1)',
+//     // position: 'absolute',
+//     // bottom: 0,
+//     transition: 'width 195ms cubic-bezier(0.4, 0, 0.6, 1) 0ms, margin-left 195ms cubic-bezier(0.4, 0, 0.6, 1) 0ms',
+//   };
+//   return { footerStyles };
+// };
+
 export const footerStyles = {
   display: 'flex',
   justifyContent: 'space-between',
@@ -5,11 +28,11 @@ export const footerStyles = {
   height: '60px',
   //   width: 'calc(100% - 64px)',
   width: '100%',
-  marginLeft: '-24px',
+  // marginLeft: '60px',
   backgroundColor: 'transparent',
   boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.1)',
-  position: 'absolute',
-  bottom: 0,
+  // position: 'absolute',
+  // bottom: 0,
 };
 
 export const footerLeftGroupStyles = {
@@ -17,11 +40,18 @@ export const footerLeftGroupStyles = {
   display: 'flex',
   gap: '12px',
   alignItems: 'center',
-  marginLeft: '24px',
+  marginLeft: {
+    xs: '8px',
+    md: '24px',
+  },
 };
 
 export const footerRightGroupStyles = {
-  marginRight: '24px',
+  marginRight: {
+    xs: '8px',
+    md: '24px',
+  },
+  textAlign: 'right',
 };
 
 export const linkStyles = {
--- src/components/stylingComponents/PageWrapper/PageWrapper.tsx
+++ src/components/stylingComponents/PageWrapper/PageWrapper.tsx
@@ -11,17 +11,23 @@ const PageWrapper = ({ children }: PropsWithChildren) => {
       md: isDrawerOpen ? '240px' : '64px',
     },
     transition: 'margin-left 195ms cubic-bezier(0.4, 0, 0.6, 1) 0ms',
-    padding: {
-      xs: '8px',
-      sm: '12px',
-      md: '16px',
-      lg: '24px',
-    },
+    // padding: {
+    //   xs: '8px',
+    //   sm: '12px',
+    //   md: '16px',
+    //   lg: '24px',
+    // },
     display: 'flex',
     flexDirection: 'column',
-    position: 'relative',
-    minHeight: 'calc(100vh - 60px)',
-    paddingBottom: '60px !important',
+    justifyContent: 'space-between',
+    // position: 'relative',
+    minHeight: {
+      xs: 'calc(100vh - 56px)',
+      // sm: 'calc(100vh - 56px)',
+      md: 'calc(100vh - 64px)',
+    },
+
+    // paddingBottom: '60px !important',
   };
   return <Box sx={wrapperStyles}>{children}</Box>;
 };
