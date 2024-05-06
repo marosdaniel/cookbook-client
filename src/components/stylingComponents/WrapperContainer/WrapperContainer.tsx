f72d5fd2f61d113beef546d9332415cf195e40d0
Maros Dániel <marosdaniel@gmail.com>
2024-05-06 12:07:58 +0200

    small improvement

--- src/components/stylingComponents/WrapperContainer/WrapperContainer.tsx
+++ src/components/stylingComponents/WrapperContainer/WrapperContainer.tsx
@@ -3,13 +3,13 @@ import { Container } from '@mui/material';
 import { IProps } from './types';
 import { containerStyles } from './styles';
 
-const WrapperContainer = ({ children, id, additionalStyles, maxWidth = 'lg' }: PropsWithChildren<IProps>) => {
+const WrapperContainer = ({ children, id, additionalStyles, maxWidth }: PropsWithChildren<IProps>) => {
   const styles = {
     ...containerStyles,
     ...additionalStyles,
   };
   return (
-    <Container component="section" id={id} sx={styles} maxWidth={maxWidth}>
+    <Container component="section" id={id} sx={styles} maxWidth={maxWidth ?? 'lg'}>
       {children}
     </Container>
   );
