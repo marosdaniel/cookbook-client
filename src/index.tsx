b39b3cee00cf795e9112867f4e5a19c9cf83a098
Maros Dániel <marosdaniel@gmail.com>
2024-05-15 10:55:00 +0200

    remove web-vitals 2

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,6 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import App from './App';
 import theme from './theme/';
 import { persistor, store } from './store';
-import reportWebVitals from './reportWebVitals';
 import { client } from './utils/graphqlClientConfig';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
@@ -28,8 +27,3 @@ root.render(
     </ApolloProvider>
   </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
